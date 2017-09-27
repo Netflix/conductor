bcde4eeccab02c220679512a8ab79fab661aa546
Oleksiy Lysak <olysak@luxoft.com>
2017-09-27 17:30:41 +0300

    Applied 'direct' mode for WorkflowSweeper module. Still in testing

--- core/src/main/java/com/netflix/conductor/core/execution/WorkflowSweeper.java
+++ core/src/main/java/com/netflix/conductor/core/execution/WorkflowSweeper.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Netflix, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,29 +14,28 @@
  * limitations under the License.
  */
 /**
- * 
+ *
  */
 package com.netflix.conductor.core.execution;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.netflix.conductor.core.WorkflowContext;
 import com.netflix.conductor.core.config.Configuration;
 import com.netflix.conductor.core.execution.ApplicationException.Code;
+import com.netflix.conductor.dao.MetadataDAO;
 import com.netflix.conductor.dao.QueueDAO;
 import com.netflix.conductor.metrics.Monitors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.*;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author Viren
@@ -46,95 +45,110 @@ import com.netflix.conductor.metrics.Monitors;
 @Singleton
 public class WorkflowSweeper {
 
-	private static Logger logger = LoggerFactory.getLogger(WorkflowSweeper.class);
+    private static Logger logger = LoggerFactory.getLogger(WorkflowSweeper.class);
 
-	private ExecutorService es;
-	
-	private Configuration config;
-	
-	private QueueDAO queues;
-	
-	private int executorThreadPoolSize;
-	
-	private static final String className = WorkflowSweeper.class.getSimpleName();
-		
-	@Inject
-	public WorkflowSweeper(WorkflowExecutor executor, Configuration config, QueueDAO queues) {
-		this.config = config;
-		this.queues = queues;
-		this.executorThreadPoolSize = config.getIntProperty("workflow.sweeper.thread.count", 5);
-		if(this.executorThreadPoolSize > 0) {
-			this.es = Executors.newFixedThreadPool(executorThreadPoolSize);
-			init(executor);
-			logger.info("Workflow Sweeper Initialized");
-		} else {
-			logger.warn("Workflow sweeper is DISABLED");
-		}
-		
-	}
+    private ExecutorService es;
 
-	public void init(WorkflowExecutor executor) {
-		
-		ScheduledExecutorService deciderPool = Executors.newScheduledThreadPool(1);
-		
-		deciderPool.scheduleWithFixedDelay(() -> {
+    private Configuration config;
 
-			try{
-				boolean disable = config.disableSweep();
-				if (disable) {
-					logger.info("Workflow sweep is disabled.");
-					return;
-				}
-				List<String> workflowIds = queues.pop(WorkflowExecutor.deciderQueue, 2 * executorThreadPoolSize, 2000);
-				sweep(workflowIds, executor);
-				
-			}catch(Exception e){
-				Monitors.error(className, "sweep");
-				logger.error(e.getMessage(), e);
-				
-			}
-			
-			
-		}, 500, 500, TimeUnit.MILLISECONDS);
-	}
+    private QueueDAO queues;
 
-	public void sweep(List<String> workflowIds, WorkflowExecutor executor) throws Exception {
-		
-		List<Future<?>> futures = new LinkedList<>();
-		for (String workflowId : workflowIds) {
-			Future<?> future = es.submit(() -> {
-				try {
-					
-					WorkflowContext ctx = new WorkflowContext(config.getAppId());
-					WorkflowContext.set(ctx);
-					if(logger.isDebugEnabled()) {
-						logger.debug("Running sweeper for workflow {}", workflowId);
-					}
-					boolean done = executor.decide(workflowId);
-					if(!done) {
-						queues.setUnackTimeout(WorkflowExecutor.deciderQueue, workflowId, config.getSweepFrequency() * 1000);
-					} else {
-						queues.remove(WorkflowExecutor.deciderQueue, workflowId);
-					}
-					
-				} catch (ApplicationException e) {
-					if(e.getCode().equals(Code.NOT_FOUND)) {
-						logger.error("Workflow NOT found for id: " + workflowId, e);
-						queues.remove(WorkflowExecutor.deciderQueue, workflowId);
-					}
-					
-				} catch (Exception e) {
-					Monitors.error(className, "sweep");
-					logger.error("Error running sweep for " + workflowId, e);
-				}
-			});
-			futures.add(future);
-		}
+    private int executorThreadPoolSize;
 
-		for (Future<?> future : futures) {
-			future.get();
-		}
-	
-	}
-	
+    private static final String className = WorkflowSweeper.class.getSimpleName();
+
+    private static final String MODE_DIRECT = "direct";
+
+    @Inject
+    public WorkflowSweeper(WorkflowExecutor executor, MetadataDAO metadata, Configuration config, QueueDAO queues) {
+        this.config = config;
+        this.queues = queues;
+        this.executorThreadPoolSize = config.getIntProperty("workflow.sweeper.thread.count", 5);
+        int delaySeconds = config.getIntProperty("workflow.sweeper.delay.seconds", 1);
+        String mode = config.getProperty("workflow.sweeper.mode", null);
+        if (this.executorThreadPoolSize > 0) {
+            this.es = Executors.newFixedThreadPool(executorThreadPoolSize);
+            init(executor, metadata, mode, delaySeconds);
+            logger.info("Workflow Sweeper Initialized");
+        } else {
+            logger.warn("Workflow sweeper is DISABLED");
+        }
+    }
+
+    public void init(WorkflowExecutor executor, MetadataDAO metadata, String mode, int delaySeconds) {
+
+        ScheduledExecutorService deciderPool = Executors.newScheduledThreadPool(1);
+
+        deciderPool.scheduleWithFixedDelay(() -> {
+
+            try {
+                boolean disable = config.disableSweep();
+                if (disable) {
+                    logger.info("Workflow sweep is disabled.");
+                    return;
+                }
+
+                if (MODE_DIRECT.equals(mode)) {
+                    List<String> workflowIds = metadata.getAll().stream().map(workflowDef -> {
+                        try {
+                            return executor.getRunningWorkflowIds(workflowDef.getName());
+                        } catch (Exception e) {
+                            logger.error("Unable to get running workflow ids for "
+                                    + workflowDef.getName() + ": " + e.getMessage(), e);
+                            return null;
+                        }
+                    }).filter(Objects::nonNull).flatMap(Collection::stream).collect(toList());
+                    sweep(workflowIds, executor, false);
+                } else {
+                    List<String> workflowIds = queues.pop(WorkflowExecutor.deciderQueue, 2 * executorThreadPoolSize, 2000);
+                    sweep(workflowIds, executor, true);
+                }
+
+            } catch (Exception e) {
+                Monitors.error(className, "sweep");
+                logger.error(e.getMessage(), e);
+            }
+
+        }, 500, delaySeconds * 1000, TimeUnit.MILLISECONDS);
+    }
+
+    public void sweep(List<String> workflowIds, WorkflowExecutor executor, boolean isUnack) throws Exception {
+
+        List<Future<?>> futures = new LinkedList<>();
+        for (String workflowId : workflowIds) {
+            Future<?> future = es.submit(() -> {
+                try {
+
+                    WorkflowContext ctx = new WorkflowContext(config.getAppId());
+                    WorkflowContext.set(ctx);
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Running sweeper for workflow {}", workflowId);
+                    }
+                    boolean done = executor.decide(workflowId);
+                    if (!done && isUnack) {
+                        queues.setUnackTimeout(WorkflowExecutor.deciderQueue, workflowId, config.getSweepFrequency() * 1000);
+                    } else {
+                        queues.remove(WorkflowExecutor.deciderQueue, workflowId);
+                    }
+
+                } catch (ApplicationException e) {
+                    if (e.getCode().equals(Code.NOT_FOUND)) {
+                        logger.error("Workflow NOT found for id: " + workflowId, e);
+                        queues.remove(WorkflowExecutor.deciderQueue, workflowId);
+                    }
+
+                } catch (Exception e) {
+                    Monitors.error(className, "sweep");
+                    logger.error("Error running sweep for " + workflowId, e);
+                }
+            });
+            futures.add(future);
+        }
+
+        for (Future<?> future : futures) {
+            future.get();
+        }
+
+    }
+
 }
