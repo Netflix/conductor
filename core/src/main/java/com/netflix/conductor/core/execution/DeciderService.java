714fc89d655a5b80f68d472750ff649f54bc24ff
Oleksiy Lysak <olysak@luxoft.com>
2017-10-17 15:10:30 +0300

    correlationId has been added

--- core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
+++ core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
@@ -135,6 +135,7 @@ public class DeciderService {
 						Map<String, Object> params = new HashMap<>();
 						params.put("workflowId", workflow.getWorkflowId());
 						params.put("workflowType", workflow.getWorkflowType());
+						params.put("correlationId", workflow.getCorrelationId());
 						params.put("workflowInput", workflow.getInput());
 						params.put("workflowVersion", workflow.getVersion());
 						params.put("taskId", task.getTaskId());
