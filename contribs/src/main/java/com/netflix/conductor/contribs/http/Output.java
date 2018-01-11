8b7dac991b384ebc04cc8c695885fb7aec50a592
sangeetanadgir <Sangeeta.Nadgir@bydeluxe.com>
2018-01-11 14:17:30 +0530

    #HTTP validation updated in previous version code

---
+++ contribs/src/main/java/com/netflix/conductor/contribs/http/Output.java
@@ -0,0 +1,45 @@
+package com.netflix.conductor.contribs.http;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Output {
+
+
+
+	private Map<String, String> conditions;
+
+	private String reasonParameter;
+
+
+	public Map<String, String> getConditions() {
+		return conditions;
+	}
+
+	/**
+	 * @param conditions the method to set
+	 */
+	public void setConditions(Map<String, String> conditions) {
+		this.conditions = conditions;
+	}
+
+
+	/**
+	 * @return the reasonParameter
+	 */
+	public String getReasonParameter() {
+		return reasonParameter;
+	}
+
+	/**
+	 * @param reasonParameter the reasonParameter to set
+	 */
+	public void setReasonParameter(String reasonParameter) {
+		this.reasonParameter = reasonParameter;
+	}
+
+}
