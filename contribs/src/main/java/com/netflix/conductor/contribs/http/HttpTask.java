385e1ccf378ea1fcc4d54231a7fb67bbef18dc7f
Oleksiy Lysak <olysak@luxoft.com>
2018-01-16 15:58:03 +0200

    OWCOND-199 - Minor code changes

--- contribs/src/main/java/com/netflix/conductor/contribs/http/HttpTask.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/http/HttpTask.java
@@ -25,15 +25,19 @@ import com.netflix.conductor.common.metadata.tasks.Task.Status;
 import com.netflix.conductor.common.run.Workflow;
 import com.netflix.conductor.core.config.Configuration;
 import com.netflix.conductor.core.execution.WorkflowExecutor;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
+
 import com.netflix.conductor.core.events.ScriptEvaluator;
+
 /**
  * @author Viren
  * Task that enables calling another http endpoint as part of its execution
@@ -108,20 +112,12 @@ public class HttpTask extends GenericHttpTask {
 				response = httpCall(input);
 			}
 
-			if (response.statusCode > 199 && response.statusCode < 300) {
-
-			// Check the http response validation
-				if (!checkHttpResponseValidation(task,response)) {
-				if (response != null) {
-					task.getOutputData().put("response", response.asMap());
-				}
-				String overallReason = "Payload validation failed";
-				task.setReasonForIncompletion(overallReason);
-				task.setStatus(Status.FAILED);
-			} else {
-				task.setStatus(Status.COMPLETED);
-			}
 			logger.info("http task execution completed.workflowId=" + workflow.getWorkflowId() + ",CorrelationId=" + workflow.getCorrelationId() + ",taskId=" + task.getTaskId() + ",taskreference name=" + task.getReferenceTaskName() + ",response code=" + response.statusCode + ",response=" + response.body);
+			if (response.statusCode > 199 && response.statusCode < 300) {
+				task.setStatus(Status.COMPLETED);
+
+				// Check the http response validation. It will overwrite the task status if needed
+				checkHttpResponseValidation(task, response);
 			} else {
 				if (response.body != null) {
 					task.setReasonForIncompletion(response.body.toString());
@@ -163,9 +159,10 @@ public class HttpTask extends GenericHttpTask {
 		return 60;
 	}
 
+	@SuppressWarnings("unchecked")
 	private void addEvalResult(Task task, String condition, Object result) {
 		Map<String, Object> taskOutput = task.getOutputData();
-		Map<String, Object> conditions = (Map<String, Object>)taskOutput.get(CONDITIONS_PARAMETER);
+		Map<String, Object> conditions = (Map<String, Object>) taskOutput.get(CONDITIONS_PARAMETER);
 		if (conditions == null) {
 			conditions = new HashMap<>();
 			taskOutput.put(CONDITIONS_PARAMETER, conditions);
@@ -173,47 +170,58 @@ public class HttpTask extends GenericHttpTask {
 		conditions.put(condition, result);
 	}
 
+	private void checkHttpResponseValidation(Task task, HttpResponse response) {
+		Object responseParam = task.getInputData().get(RESPONSE_PARAMETER_NAME);
 
-	private Boolean checkHttpResponseValidation(Task task,HttpResponse response)
-	{
-		AtomicBoolean overallStatus = new AtomicBoolean(true);
-		Object responseConditions;
-		Output output=new Output();
-		Map<String, String> conditionsObj = new HashMap<String, String>();
-		//Check http_response object is present or not
-		if (task.getInputData().get(RESPONSE_PARAMETER_NAME) == null) {   return overallStatus.get(); }
-		responseConditions = task.getInputData().get(RESPONSE_PARAMETER_NAME);
-		output = om.convertValue(responseConditions, Output.class);
-		//Check condition object is present or not
-		if (output.getConditions() == null || output.getConditions().isEmpty()) {  return overallStatus.get(); }
-		conditionsObj = output.getConditions();
-		Object payloadObj = response.asMap();
+		// Check http_response object is present or not
+		if (responseParam == null) {
+			return;
+		}
+		Output output = om.convertValue(responseParam, Output.class);
+
+		// Check condition object is present or not
+		if (output.getConditions() == null || output.getConditions().isEmpty()) {
+			return;
+		}
+
+		// Get the response map
+		Map<String, Object> responseMap = response.asMap();
+
 		// Go over all conditions and evaluate them
-			conditionsObj.forEach((name, condition) -> {
-				try {
-					Boolean success = ScriptEvaluator.evalBool(condition, payloadObj);
-					logger.debug("Evaluation resulted in " + success + " for " + name + "=" + condition);
+		AtomicBoolean overallStatus = new AtomicBoolean(true);
+		output.getConditions().forEach((name, condition) -> {
+			try {
+				Boolean success = ScriptEvaluator.evalBool(condition, responseMap);
+				logger.debug("Evaluation resulted in " + success + " for " + name + "=" + condition);
 
-					// Failed ?
-					if (!success) {
+				// Failed ?
+				if (!success) {
 
-						// Add condition evaluation result into output map
-						addEvalResult(task, name, success);
-
-						// Set the over all status to false
-						overallStatus.set(false);
-					}
-				} catch (Exception ex) {
-					logger.error("Evaluation failed for " + name + "=" + condition, ex);
-
-					// Set the error message instead of false
-					addEvalResult(task, name, ex.getMessage());
+					// Add condition evaluation result into output map
+					addEvalResult(task, name, success);
 
 					// Set the over all status to false
 					overallStatus.set(false);
 				}
-			});
+			} catch (Exception ex) {
+				logger.error("Evaluation failed for " + name + "=" + condition, ex);
 
-		return overallStatus.get();
+				// Set the error message instead of false
+				addEvalResult(task, name, ex.getMessage());
+
+				// Set the over all status to false
+				overallStatus.set(false);
+			}
+		});
+
+		// If anything failed - fail the task
+		if (!overallStatus.get()) {
+			String overallReason = "Response validation failed";
+			if (StringUtils.isNotEmpty(output.getReasonParameter())) {
+				overallReason = output.getReasonParameter();
+			}
+			task.setReasonForIncompletion(overallReason);
+			task.setStatus(Status.FAILED);
+		}
 	}
 }
