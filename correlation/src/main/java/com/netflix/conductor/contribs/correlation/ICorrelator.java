f18ced0dd8f20a8312542887fee38138f7610325
Oleksiy Lysak <lysakaleksey@restrixio.com>
2018-01-22 13:04:16 +0200

    OWCOND-173 (Get Header info from Start Workflow)
    Removed unnecessary import

--- correlation/src/main/java/com/netflix/conductor/contribs/correlation/ICorrelator.java
+++ correlation/src/main/java/com/netflix/conductor/contribs/correlation/ICorrelator.java
@@ -2,7 +2,6 @@ package com.netflix.conductor.contribs.correlation;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sun.jersey.api.client.WebResource;
-import sun.net.www.http.HttpClient;
 
 /**
  * Created by beimforz on 12/21/17.
