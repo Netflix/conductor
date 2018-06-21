22ca1761aab2fbf64d42f24c25bcbd5f10346fdc
Brian Schroeder <bschroeder@ccctechcenter.org>
2018-06-21 16:37:39 -0700

    Fixed injection problem and updated .gitignore

--- es5-persistence/src/main/java/com/netflix/conductor/dao/es5/index/CustomHighLevelRestClient.java
+++ es5-persistence/src/main/java/com/netflix/conductor/dao/es5/index/CustomHighLevelRestClient.java
@@ -3,13 +3,17 @@ package com.netflix.conductor.dao.es5.index;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 
+import javax.inject.Inject;
+
 public class CustomHighLevelRestClient extends RestHighLevelClient{
     public RestClient restClient;
 
+    @Inject
     public CustomHighLevelRestClient(RestClient restClient) {
         super(restClient);
         this.restClient = restClient;
     }
+
 
     public RestClient getLowLevelRestClient() {
         return this.restClient;
