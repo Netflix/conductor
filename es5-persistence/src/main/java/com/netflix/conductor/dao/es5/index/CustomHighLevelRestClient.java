d957b7dbc377f056b36e83e8176c18398a7dc5d9
Brian Schroeder <bschroeder@ccctechcenter.org>
2018-06-22 10:58:34 -0700

    Guice injection tweeks

--- es5-persistence/src/main/java/com/netflix/conductor/dao/es5/index/CustomHighLevelRestClient.java
+++ es5-persistence/src/main/java/com/netflix/conductor/dao/es5/index/CustomHighLevelRestClient.java
@@ -3,12 +3,9 @@ package com.netflix.conductor.dao.es5.index;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 
-import javax.inject.Inject;
-
 public class CustomHighLevelRestClient extends RestHighLevelClient{
     public RestClient restClient;
 
-    @Inject
     public CustomHighLevelRestClient(RestClient restClient) {
         super(restClient);
         this.restClient = restClient;
