10152c01afef6ea1f36106b727deb4838b519c9b
Sean Willison <sean.willison@gmail.com>
2018-07-31 16:08:01 -0400

    formatting and linting only

--- ui/src/components/workflow/tasks/TasksMetaList.js
+++ ui/src/components/workflow/tasks/TasksMetaList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Input, Popover, OverlayTrigger } from 'react-bootstrap';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { connect } from 'react-redux';
 import { getTaskDefs } from '../../../actions/WorkflowActions';
 
@@ -10,60 +10,129 @@ const TaskMetaList = React.createClass({
       name: '',
       version: '',
       taskDefs: []
-    }
+    };
   },
-  componentWillMount(){
+  componentWillMount() {
     this.props.dispatch(getTaskDefs());
   },
-  componentWillReceiveProps(nextProps){
+  componentWillReceiveProps(nextProps) {
     this.state.taskDefs = nextProps.taskDefs;
-
   },
   render() {
     var wfs = this.state.taskDefs;
 
-    function retries(cell, row){
-      if(row.retryLogic == 'FIXED') {
+    function retries(cell, row) {
+      if (row.retryLogic == 'FIXED') {
         return row.retryLogic + ' (' + row.retryDelaySeconds + ' seconds)';
       }
-    };
+    }
 
-    function editor(cell, row){
-      return (<OverlayTrigger trigger="click" rootClose placement="right" overlay={
-        <Popover title={row.name} style={{ width: '500px'}}><div className="left">
-          <form>
-            <Input type="text" ref="retryCount" value={row.retryCount} addonBefore="Retry Count" addonAfter="Times"></Input><br/>
-            <Input type="select" ref="retryLogic" value={row.retryLogic} addonBefore="Retry Logic">
-                <option value="FIXED">FIXED</option>
-                <option value="EXPONENTIAL_BACKOFF">EXPONENTIAL_BACKOFF</option>
-            </Input><br/>
-            <Input type="text" ref="retryDelaySeconds" value={row.retryDelaySeconds} addonBefore="Retry Delay" addonAfter="Seconds"></Input><br/>
-            <Input type="select" ref="timeoutPolicy" value={row.timeoutPolicy} addonBefore="Time Out Action">
-              <option value="RETRY_TASK">RETRY TASK</option>
-              <option value="TIME_OUT_WF">TIME_OUT_WF</option>
-            </Input><br/>
-            <Input type="text" ref="timeoutSeconds" value={row.timeoutSeconds} addonBefore="Time Out" addonAfter="Seconds"></Input><br/>
-            <Input type="text" ref="restimeoutSeconds" value={row.responseTimeoutSeconds} addonBefore="Response Time Out" addonAfter="timeoutSeconds"></Input><br/>
-            <Input type="text" ref="concurrentExecLimit" value={row.concurrentExecLimit} addonBefore="Concurrent Exec Limit"></Input><br/>
-            <Input type="textarea" label="Task Description" ref="description" value={row.description} readonly={true}/><br/>
-          </form>
-        </div></Popover>
-      }><a>{cell}</a></OverlayTrigger>);
-    };
+    function editor(cell, row) {
+      return (
+        <OverlayTrigger
+          trigger="click"
+          rootClose
+          placement="right"
+          overlay={
+            <Popover title={row.name} style={{ width: '500px' }}>
+              <div className="left">
+                <form>
+                  <Input
+                    type="text"
+                    ref="retryCount"
+                    value={row.retryCount}
+                    addonBefore="Retry Count"
+                    addonAfter="Times"
+                  />
+                  <br />
+                  <Input type="select" ref="retryLogic" value={row.retryLogic} addonBefore="Retry Logic">
+                    <option value="FIXED">FIXED</option>
+                    <option value="EXPONENTIAL_BACKOFF">EXPONENTIAL_BACKOFF</option>
+                  </Input>
+                  <br />
+                  <Input
+                    type="text"
+                    ref="retryDelaySeconds"
+                    value={row.retryDelaySeconds}
+                    addonBefore="Retry Delay"
+                    addonAfter="Seconds"
+                  />
+                  <br />
+                  <Input type="select" ref="timeoutPolicy" value={row.timeoutPolicy} addonBefore="Time Out Action">
+                    <option value="RETRY_TASK">RETRY TASK</option>
+                    <option value="TIME_OUT_WF">TIME_OUT_WF</option>
+                  </Input>
+                  <br />
+                  <Input
+                    type="text"
+                    ref="timeoutSeconds"
+                    value={row.timeoutSeconds}
+                    addonBefore="Time Out"
+                    addonAfter="Seconds"
+                  />
+                  <br />
+                  <Input
+                    type="text"
+                    ref="restimeoutSeconds"
+                    value={row.responseTimeoutSeconds}
+                    addonBefore="Response Time Out"
+                    addonAfter="timeoutSeconds"
+                  />
+                  <br />
+                  <Input
+                    type="text"
+                    ref="concurrentExecLimit"
+                    value={row.concurrentExecLimit}
+                    addonBefore="Concurrent Exec Limit"
+                  />
+                  <br />
+                  <Input
+                    type="textarea"
+                    label="Task Description"
+                    ref="description"
+                    value={row.description}
+                    readonly={true}
+                  />
+                  <br />
+                </form>
+              </div>
+            </Popover>
+          }
+        >
+          <a>{cell}</a>
+        </OverlayTrigger>
+      );
+    }
 
     return (
       <div className="ui-content">
         <h1>Task Definitions</h1>
         <BootstrapTable data={wfs} striped={true} hover={true} search={true} exportCSV={false} pagination={false}>
-          <TableHeaderColumn dataField="name" isKey={true} dataAlign="left" dataSort={true} dataFormat={editor}>Name/Version</TableHeaderColumn>
-          <TableHeaderColumn dataField="ownerApp" dataSort={true} >Owner App</TableHeaderColumn>
-          <TableHeaderColumn dataField="timeoutPolicy" dataSort={true} >Timeout Policy</TableHeaderColumn>
-          <TableHeaderColumn dataField="timeoutSeconds" dataSort={true} >Timeout Seconds</TableHeaderColumn>
-          <TableHeaderColumn dataField="responseTimeoutSeconds" dataSort={true} >Response Timeout Seconds</TableHeaderColumn>
-          <TableHeaderColumn dataField="retryCount" dataSort={true} >Retry Count</TableHeaderColumn>
-          <TableHeaderColumn dataField="concurrentExecLimit" dataSort={true} >Concurrent Exec Limit</TableHeaderColumn>
-          <TableHeaderColumn dataField="retryLogic" dataSort={true} dataFormat={retries}>Retry Logic</TableHeaderColumn>
-          </BootstrapTable>
+          <TableHeaderColumn dataField="name" isKey={true} dataAlign="left" dataSort={true} dataFormat={editor}>
+            Name/Version
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="ownerApp" dataSort={true}>
+            Owner App
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="timeoutPolicy" dataSort={true}>
+            Timeout Policy
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="timeoutSeconds" dataSort={true}>
+            Timeout Seconds
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="responseTimeoutSeconds" dataSort={true}>
+            Response Timeout Seconds
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="retryCount" dataSort={true}>
+            Retry Count
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="concurrentExecLimit" dataSort={true}>
+            Concurrent Exec Limit
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="retryLogic" dataSort={true} dataFormat={retries}>
+            Retry Logic
+          </TableHeaderColumn>
+        </BootstrapTable>
       </div>
     );
   }
