f8ad0805ac8a40920b89e48b1d7e6e11fc672269
Jozef Rozsa <jrozsa@frinx.io>
2018-08-08 14:25:23 +0200

    Fix task outpout and remove log on failed task
    Signed-off-by: Jozef Rozsa <jrozsa@frinx.io>

--- polyglot-clients/python/conductor/ConductorWorker.py
+++ polyglot-clients/python/conductor/ConductorWorker.py
@@ -120,8 +120,7 @@ class ConductorWorker:
         except Exception as err:
             print(f'Error executing task: {exec_function.__name__} with error: {str(err)}')
             task['status'] = 'FAILED'
-            task['logs'] = str(err)
-            task['outputData'] = str(err)
+            task['outputData'] = {'error-message' : str(err) }
             self.taskClient.updateTask(task)
 
     def poll_and_execute(self, taskType, exec_function, domain=None):
