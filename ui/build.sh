e4d6c4661e12ec9512f7c50862463b21db490c0e
Sean Willison <sean.willison@gmail.com>
2018-08-21 16:17:16 -0400

    cleaning up UI build to move towards a react-scripts build

--- ui/build.sh
+++
@@ -1,2 +0,0 @@
-rm -rf ./dist | true
-bable
--- ui/src/index.js
+++ ui/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 
-import 'index.css';
+import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/bootstrap-theme.min.css';
+import 'index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
