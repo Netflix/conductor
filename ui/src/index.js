da320ad5c8b8eaa982337799a682b7d4e63075a9
Sean Willison <sean.willison@gmail.com>
2018-08-21 18:47:37 -0400

    builds and styles working

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,6 +5,7 @@ import App from 'App';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/bootstrap-theme.min.css';
+import 'styles/react-bootstrap-table-all.css';
 import 'index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
