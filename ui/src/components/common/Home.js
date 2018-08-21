a06bfc704c1326faf28bf08553d8706808493bae
Sean Willison <sean.willison@gmail.com>
2018-08-21 18:36:57 -0400

    builds and styles working

--- ui/src/components/common/Home.js
+++ ui/src/components/common/Home.js
@@ -1,23 +1,16 @@
 import { Jumbotron } from 'react-bootstrap';
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 
-class Introduction extends Component {
+import netflixImg from 'images/conductor.png';
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-        <Jumbotron className="jumbotron">
-          <div className="row">
-            <img src="/images/conductor.png"></img>
-          </div>
-          <div className="row">&nbsp;</div>
-        </Jumbotron>
-    );
-  }
-}
+const Introduction = () => (
+  <Jumbotron className="jumbotron">
+    <div className="row">
+      <img src={netflixImg} />
+    </div>
+    <div className="row">&nbsp;</div>
+  </Jumbotron>
+);
 
 export default connect(state => state)(Introduction);
