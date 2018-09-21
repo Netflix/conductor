0bf89efa3996b64d60cde991ac0073b1be3e7d46
Oleksiy Lysak <olysak@luxoft.com>
2018-09-21 15:05:19 +0300

    ONECOND-563 Update the Wait Task to Include Additional Info. Do not fail for empty value

--- contribs/src/main/java/com/netflix/conductor/contribs/asset/AssetMonitor.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/asset/AssetMonitor.java
@@ -195,8 +195,15 @@ public class AssetMonitor implements JavaEventAction {
 
 			// Skip if any of the messageParameters does not match the workflowParameters
 			boolean anyNotEqual = messageParameters.entrySet().stream().anyMatch(entry -> {
-				String value = workflowParameters.get(entry.getKey());
-				return !entry.getValue().equalsIgnoreCase(value);
+				String wfValue = workflowParameters.get(entry.getKey());
+				String msgValue = entry.getValue();
+				if (StringUtils.isEmpty(wfValue) && StringUtils.isEmpty(msgValue)) {
+					return false;
+				} else if (StringUtils.isNotEmpty(msgValue)) {
+					return !msgValue.equalsIgnoreCase(wfValue);
+				} else {
+					return !wfValue.equalsIgnoreCase(msgValue);
+				}
 			});
 			if (anyNotEqual) {
 				continue;
