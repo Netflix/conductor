5407ccdec2269382f768b19f56489701f35afa4a
Oleksiy Lysak <olysak@luxoft.com>
2018-09-25 13:23:25 +0300

    ONECOND-559 - Conductor UI use incorrect task state after task is retried

--- ui/src/api/wfegraph.js
+++ ui/src/api/wfegraph.js
@@ -37,9 +37,8 @@ class Workflow2Graph {
     this.executedTasks = {};
     let joins = {};
     wfe.tasks.forEach(t => {
-      if (!this.executedTasks.hasOwnProperty(t.referenceTaskName)){
-        this.executedTasks[t.referenceTaskName] = {status: t.status, input: t.inputData, output: t.outputData, taskType: t.taskType, reasonForIncompletion: t.reasonForIncompletion, task: t};
-      }
+      console.log('t', t);
+      this.executedTasks[t.referenceTaskName] = {status: t.status, input: t.inputData, output: t.outputData, taskType: t.taskType, reasonForIncompletion: t.reasonForIncompletion, task: t};
       if(t.taskType == 'JOIN' ){
         joins[t.referenceTaskName] = t.inputData.joinOn;
       }
