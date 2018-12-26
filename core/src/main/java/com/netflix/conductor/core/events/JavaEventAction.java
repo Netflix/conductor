7431710399980bb750637ad398f53337706d8400
Oleksiy Lysak <olysak@luxoft.com>
2018-12-26 17:25:02 +0200

    ONECOND-788 - All ATLAS Handler need to new action

--- core/src/main/java/com/netflix/conductor/core/events/JavaEventAction.java
+++ core/src/main/java/com/netflix/conductor/core/events/JavaEventAction.java
@@ -2,6 +2,8 @@ package com.netflix.conductor.core.events;
 
 import com.netflix.conductor.common.metadata.events.EventHandler;
 
+import java.util.List;
+
 public interface JavaEventAction {
-	void handle(EventHandler.Action action, Object payload, String event, String messageId) throws Exception;
+	List<?> handle(EventHandler.Action action, Object payload, String event, String messageId) throws Exception;
 }
