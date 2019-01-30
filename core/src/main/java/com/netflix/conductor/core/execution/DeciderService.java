460bb5deb66ba461cab1f92b049be547be905bac
Oleksiy Lysak <olysak@luxoft.com>
2019-01-30 21:22:19 +0200

    ONECOND-817 - The parent workflow canceled by API but subworkflow status has failed status with no reason

--- core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
+++ core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
@@ -312,11 +312,12 @@ public class DeciderService {
 
 		int retryCount = task.getRetryCount();
 		if (!task.getStatus().isRetriable() || SystemTaskType.isBuiltIn(task.getTaskType()) || taskDef == null || taskDef.getRetryCount() <= retryCount) {
+			String reason = StringUtils.defaultIfEmpty(task.getReasonForIncompletion(), workflow.getReasonForIncompletion());
 			WorkflowStatus status = task.getStatus().equals(Status.TIMED_OUT) ? WorkflowStatus.TIMED_OUT :
 					task.getStatus().equals(Status.RESET) ? WorkflowStatus.RESET : WorkflowStatus.FAILED;
 			task.setRetried(true);
-			logger.error("Timeout/fail/reset error occurred. workflowId=" + workflow.getWorkflowId()+",taskId"+task.getTaskId()+",CorrelationId="+workflow.getCorrelationId()+",Reason="+task.getReasonForIncompletion()+",workflowstatus="+status);
-			throw new TerminateWorkflow(task.getReasonForIncompletion(), status, task);
+			logger.error("Timeout/fail/reset error occurred. workflowId=" + workflow.getWorkflowId()+",taskId"+task.getTaskId()+",CorrelationId="+workflow.getCorrelationId()+",Reason="+reason+",workflowstatus="+status);
+			throw new TerminateWorkflow(reason, status, task);
 		}
 
 		// retry... - but not immediately - put a delay...
