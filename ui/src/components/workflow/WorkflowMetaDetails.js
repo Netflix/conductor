8c8ab8ac03c89fd3707a3bd5a4bce1209d96c489
rgarlik <rgarlik@gmail.com>
2019-02-12 15:25:34 +0100

    Some things fixed

--- ui/src/components/workflow/WorkflowMetaDetails.js
+++ ui/src/components/workflow/WorkflowMetaDetails.js
@@ -62,7 +62,7 @@ class WorkflowMetaDetails extends Component {
             <div><WorkflowMetaDia meta={wf} tasks={[]}/></div>
           </Tab>
           <Tab ref={this.JSONTab} eventKey={2} title="JSON">
-            <JSONTab wfs={JSON.stringify(this.state.workflowMeta, null, 2)} name={this.state.name} version={this.state.version}></JSONTab>
+            <div><JSONTab wfs={JSON.stringify(this.state.workflowMeta, null, 2)} name={this.state.name} version={this.state.version} /></div>
           </Tab>
           <Tab eventKey={3} title="Input">
           <div><WorkflowMetaInput workflowForm={this.state.workflowForm} name={this.state.name}/></div>
@@ -83,6 +83,12 @@ class JSONTab extends Component {
       isNotParsable: false
     }
     this.editJSONswitch = this.editJSONswitch.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      wfs: nextProps.wfs
+    })
   }
 
   editJSONswitch(e, which) {
