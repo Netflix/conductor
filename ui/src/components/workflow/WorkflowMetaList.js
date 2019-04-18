78984e9df567b3239a86ab7a9ffed1204d8141bd
thodor <thodor@frinx.io>
2019-04-18 12:43:02 +0200

    Create favourite workflows
    Signed-off-by: thodor <thodor@frinx.io>

--- ui/src/components/workflow/WorkflowMetaList.js
+++ ui/src/components/workflow/WorkflowMetaList.js
@@ -3,13 +3,22 @@ import { Link, browserHistory } from 'react-router';
 import {Button, Col, Grid, Panel, Row, Input} from 'react-bootstrap';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import { connect } from 'react-redux';
-import { getWorkflowDefs } from '../../actions/WorkflowActions';
+import {getWorkflowDefs, updateWorkflow} from '../../actions/WorkflowActions';
 import Typeahead from "react-bootstrap-typeahead";
 
-const WorkflowMetaList = React.createClass({
+class WorkflowMetaList extends Component {
 
-    getInitialState() {
-        return {
+    constructor(props) {
+        super(props);
+        this.showAllWorkflows = this.showAllWorkflows.bind(this);
+        this.filterFavourites = this.filterFavourites.bind(this);
+        this.updateSearchValue = this.updateSearchValue.bind(this);
+        this.filterChange = this.filterChange.bind(this);
+        this.removeFavourite = this.removeFavourite.bind(this);
+        this.addFavourite = this.addFavourite.bind(this);
+        this.favouriteMaker = this.favouriteMaker.bind(this);
+
+        this.state = {
             name: '',
             version: '',
             workflows: [],
@@ -17,20 +26,20 @@ const WorkflowMetaList = React.createClass({
             labels: [],
             allLabels: [],
             search: '',
-            workflowsFilteredAndSearched: [],
-        }
-    },
+            workflowsFilteredAndSearched: []
+        };
+    }
 
-  componentWillMount(){
-    this.props.dispatch(getWorkflowDefs());
-  },
+    componentWillMount(){
+        this.props.dispatch(getWorkflowDefs());
+    }
 
     componentWillReceiveProps(nextProps){
         this.state.workflows = nextProps.workflows;
         this.state.workflowsFiltered = nextProps.workflows;
         this.state.workflowsFilteredAndSearched = nextProps.workflows;
         this.state.allLabels = this.setAllLabels(nextProps.workflows);
-    },
+    }
 
     setAllLabels(wfs) {
         var tags = [];
@@ -44,7 +53,7 @@ const WorkflowMetaList = React.createClass({
         }
         tags = _.uniq(tags);
         return tags;
-    },
+    }
 
     //Parse Description to get name of LABELS
     parseDescriptionRegex(workflow){
@@ -55,12 +64,12 @@ const WorkflowMetaList = React.createClass({
         if (!Array.isArray(labels) || !labels.length)
             return [];
         return labels[0].match(labelsPattern);
-    },
+    }
 
     handleSubmit(labels) {
         this.state.labels = labels;
         this.handleSearch();
-    },
+    }
 
     showAllWorkflows(){
         this.setState({
@@ -68,12 +77,12 @@ const WorkflowMetaList = React.createClass({
             labels: [],
             search: ''
         })
-    },
+    }
 
     filterChange(labels) {
         this.state.labels = labels;
         this.handleFilter();
-    },
+    }
 
     handleFilter() {
         var wfs = this.state.workflows;
@@ -97,16 +106,16 @@ const WorkflowMetaList = React.createClass({
         {
             workflowsFiltered: filter,
         },
-            this.handleSearch(filter)
+            () =>this.handleSearch(filter)
         );
-    },
+    }
 
     updateSearchValue(e) {
         const value = e.target.value;
         this.setState({
             search: value
-        }, this.handleSearch(null, value));
-    },
+        }, () => this.handleSearch(null, value));
+    }
 
     handleSearch(alreadyFiltered, value) {
         let searchValue = '';
@@ -135,9 +144,63 @@ const WorkflowMetaList = React.createClass({
         this.setState({
             workflowsFilteredAndSearched: filter
         });
-    },
+    }
+
+    filterFavourites() {
+        let labels = this.state.labels;
+        if (labels.includes("FAVOURITE")) {
+            var index = labels.indexOf("FAVOURITE");
+            if (index > -1) {
+                labels.splice(index, 1);
+            }
+        } else {
+            labels.push("FAVOURITE");
+        }
+        this.state.labels = labels;
+        this.handleFilter();
+    }
+
+    addFavourite(data, e) {
+        e.preventDefault();
+        let description = data.description;
+        description += ", FAVOURITE";
+        data.description = description;
+
+        let toBeSent = [data];
+
+        this.props.dispatch(updateWorkflow(toBeSent));
+
+        location.reload();
+    }
+
+    removeFavourite(data, e){
+        e.preventDefault();
+        let description = data.description;
+        if (description.includes(", FAVOURITE")) {
+            description = description.replace(", FAVOURITE","");
+        }
+        data.description = description;
+
+        let toBeSent = [data];
+        this.props.dispatch(updateWorkflow(toBeSent));
+
+        location.reload();
+    };
+
+    favouriteMaker(cell, row, e) {
+        if (cell.includes("FAVOURITE")) {
+            return <span onClick={(e) => this.removeFavourite(row, e)}>
+                <i className="fa fa-star" style={{color: '#ffcf24', cursor: 'pointer'}}/>
+            </span>
+        } else {
+            return <span onClick={(e) => this.addFavourite(row, e)}>
+                <i className="far fa-star" style={{color: '#ffcf24', cursor: 'pointer'}}/>
+            </span>
+        }
+    }
 
     render() {
+        let wfs = this.state.workflowsFilteredAndSearched;
         const options = {
             noDataText: 'Please wait for data'
         };
@@ -173,31 +236,37 @@ const WorkflowMetaList = React.createClass({
                             <Col xs={1} style={{ 'textAlign' : 'center'}}>
                                 <Button onClick={this.showAllWorkflows} className="btn">Show All</Button>
                             </Col>
+                            <Col xs={1} style={{ 'textAlign' : 'center'}}>
+                                <Button onClick={this.filterFavourites}
+                                        className={this.state.labels.includes("FAVOURITE") ? "btn btn-primary" : "btn"}>
+                                    <i className="fa fa-star" style={{color: '#ffcf24'}}/> Favourite
+                                </Button>
+                            </Col>
                             <Col xs={5}>
                                 <Typeahead ref="workflowTypes" onChange={this.filterChange} options={this.state.allLabels} placeholder="Filter" multiple={true} selected={this.state.labels}/>
                                 &nbsp;<i className="fa fa-angle-up fa-1x"/>&nbsp;&nbsp;<label className="small nobold">Filter by Workflow label</label>
                             </Col>
-                            <Col xs={6}>
+                            <Col xs={5}>
                                 <Input type="input" id="wfSearchInput" value={this.state.search} onChange={this.updateSearchValue} ref="searchWorflows" placeholder="Search"/>
                                 &nbsp;<i className="fa fa-angle-up fa-1x"/>&nbsp;&nbsp;<label className="small nobold">Search by Workflow keyword</label>
                             </Col>
                         </Row>
-
                     <form>
 
                     </form>
                     </Panel>
                 </div>
                 <div className="panel panel-default">
-                    <BootstrapTable ref="table" data={this.state.workflowsFilteredAndSearched || []} striped={true} hover={true} exportCSV={false} pagination={false} options={ options }>
+                    <BootstrapTable ref="table" data={wfs || []} striped={true} hover={true} exportCSV={false} pagination={false} options={ options }>
                         <TableHeaderColumn dataField="name" isKey={true} dataAlign="left" dataSort={true} dataFormat={nameMaker}>Name/Version</TableHeaderColumn>
+                        <TableHeaderColumn dataField="description" dataAlign="center" width="50px" dataFormat={this.favouriteMaker}>Fav</TableHeaderColumn>
                         <TableHeaderColumn dataField="description" dataFormat={labelsMaker}>Labels</TableHeaderColumn>
-                        <TableHeaderColumn dataField="inputParameters" width="500" dataSort={true} dataFormat={jsonMaker}>Input Parameters</TableHeaderColumn>
+                        <TableHeaderColumn dataField="inputParameters" width="500px" dataSort={true} dataFormat={jsonMaker}>Input Parameters</TableHeaderColumn>
                         <TableHeaderColumn dataField="tasks" hidden={false} dataFormat={taskMaker}>Tasks</TableHeaderColumn>
                     </BootstrapTable>
                 </div>
             </div>
         );
     }
-});
+}
 export default connect(state => state.workflow)(WorkflowMetaList);
