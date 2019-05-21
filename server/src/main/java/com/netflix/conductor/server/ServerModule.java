a4ef1d9b7dd96e2b978e0de50bf40dbf2138c772
Oleksiy Lysak <olysak@luxoft.com>
2019-05-21 16:29:21 +0300

    ONECOND-587 Integrate conductor with DynamoDB/Aurora

--- server/src/main/java/com/netflix/conductor/server/ServerModule.java
+++ server/src/main/java/com/netflix/conductor/server/ServerModule.java
@@ -20,6 +20,7 @@ package com.netflix.conductor.server;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.netflix.conductor.aurora.AuroraModule;
 import com.netflix.conductor.contribs.AssetModule;
 import com.netflix.conductor.contribs.AuthModule;
 import com.netflix.conductor.contribs.HttpModule;
@@ -87,13 +88,13 @@ public class ServerModule extends AbstractModule {
 		bind(Configuration.class).toInstance(config);
 		bind(Registry.class).toInstance(registry);
 
-
+		install(new AuroraModule());
 		if (ConductorServer.DB.elasticsearch.equals(db)) {
 			install(new Elasticsearch6RestModule());
 			bind(IndexDAO.class).to(Elasticsearch6RestIndexDAO.class);
 
-			bind(ExecutionDAO.class).to(Elasticsearch6RestExecutionDAO.class);
-			bind(MetadataDAO.class).to(Elasticsearch6RestMetadataDAO.class);
+//			bind(ExecutionDAO.class).to(Elasticsearch6RestExecutionDAO.class);
+//			bind(MetadataDAO.class).to(Elasticsearch6RestMetadataDAO.class);
 			bind(QueueDAO.class).to(Elasticsearch6RestQueueDAO.class);
 			bind(MetricsDAO.class).to(Elasticsearch6RestMetricsDAO.class);
 		} else {
