34b6f1c4531fa8636f999d83ac77dfb0c6f020a9
Oleksiy Lysak <lysakaleksey@restrixio.com>
2019-06-14 21:27:35 +0300

    ONECOND-589 Migrate data to DynamoDB/Aurora (#546)

--- es2pg-migration/src/main/java/com/bydeluxe/es2pg/migration/Main.java
+++ es2pg-migration/src/main/java/com/bydeluxe/es2pg/migration/Main.java
@@ -81,8 +81,9 @@ public class Main {
 		poolConfig.setUsername(config.auroraUser());
 		poolConfig.setPassword(config.auroraPassword());
 		poolConfig.setAutoCommit(false);
-		poolConfig.setMaximumPoolSize(1000);
-		poolConfig.setConnectionTimeout(60_000);
+		poolConfig.addDataSourceProperty("cachePrepStmts", "true");
+		poolConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+		poolConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
 
 		logger.info("Creating PG client");
 		dataSource = new HikariDataSource(poolConfig);
