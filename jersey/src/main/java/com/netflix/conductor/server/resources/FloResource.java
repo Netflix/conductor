5380307124b10dd2ecfff4a80933072bb9684978
naveen.chlsn <chalasaninaveenz@gmail.com>
2019-06-14 15:05:12 +0530

    Adds distribution tracingId

--- jersey/src/main/java/com/netflix/conductor/server/resources/FloResource.java
+++ jersey/src/main/java/com/netflix/conductor/server/resources/FloResource.java
@@ -2,7 +2,9 @@ package com.netflix.conductor.server.resources;
 
 import com.google.common.base.Preconditions;
 import com.netflix.conductor.common.metadata.tasks.CompleteTaskResult;
+import com.netflix.conductor.core.utils.IDGenerator;
 import com.netflix.conductor.service.FloService;
+import com.netflix.conductor.service.WorkflowService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -12,11 +14,14 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.util.Map;
 
 @Api(value = "/flo", produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON, tags = "Flo APIs")
 @Path("/flo")
@@ -25,11 +30,15 @@ import javax.ws.rs.core.MediaType;
 @Singleton
 public class FloResource {
 
+    public static final String TRACING_ID = "tracingId";
     private final FloService floService;
 
+    private final WorkflowService workflowService;
+
     @Inject
-    public FloResource(FloService floService) {
+    public FloResource(FloService floService,WorkflowService workflowService) {
         this.floService = floService;
+        this.workflowService = workflowService;
     }
 
     //TODO: Replace with PUT once Reminder service supports PUT/Perf is done, whichever is earlier
@@ -47,5 +56,21 @@ public class FloResource {
         Preconditions.checkArgument(StringUtils.isNotBlank(unblockedBy), "unblockedBy cannot be null or empty.");
 
         return floService.completeWaitStateTask(workflowId, instanceState, unblockedBy);
+    }
+
+
+    @POST
+    @Path("/workflow/{name}")
+    @Produces({MediaType.TEXT_PLAIN})
+    @ApiOperation("Start a new workflow. Returns the ID of the workflow instance that can be later used for tracking")
+    public String startWorkflow(@PathParam("name") String name,
+                                @QueryParam("version") Integer version,
+                                @QueryParam("correlationId") String correlationId,@HeaderParam("x-TracingId") String tracingId,
+                                Map<String, Object> input) {
+        if(StringUtils.isBlank(tracingId)) {
+            tracingId = IDGenerator.generate();
+        }
+        input.put(TRACING_ID, tracingId);
+        return workflowService.startWorkflow(name, version, correlationId, input);
     }
 }
