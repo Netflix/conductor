e572f25dd761a4c6f880b3b5e7d22d2cfc6961f5
Oleksiy Lysak <olysak@luxoft.com>
2019-08-06 11:49:11 +0300

    ONECOND-587 Integrate conductor with DynamoDB/Aurora

--- postgresql-persistence/src/main/java/com/netflix/conductor/aurora/AuroraQueueDAO.java
+++ postgresql-persistence/src/main/java/com/netflix/conductor/aurora/AuroraQueueDAO.java
@@ -253,12 +253,12 @@ public class AuroraQueueDAO extends AuroraBaseDAO implements QueueDAO {
 		}
 
 		// Otherwise make it visible right away
-		// Also reset the version to be picked up earlier. See `pop` order by
 		final String UPDATE = "UPDATE queue_message " +
-			"SET popped = false, deliver_on = now(), unack_on = null, unacked = false, version = 0 " +
+			"SET popped = false, deliver_on = ?, unack_on = null, unacked = false, version = version + 1 " +
 			"WHERE id = ? AND version = ?";
 
-		return queryWithTransaction(UPDATE, q -> q.addParameter(record.id)
+		return queryWithTransaction(UPDATE, q -> q.addTimestampParameter(1L)
+			.addParameter(record.id)
 			.addParameter(record.version)
 			.executeUpdate()) > 0;
 	}
