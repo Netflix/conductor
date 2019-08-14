e928271722e9379791ef601feb2b7f576ad3df45
Oleksiy Lysak <olysak@luxoft.com>
2019-08-14 15:13:12 +0300

    ONECOND-1204 /v1/metrics - update subjectname for vfs_restore_bucket_name

--- postgresql-persistence/src/main/java/com/netflix/conductor/aurora/AuroraMetricsDAO.java
+++ postgresql-persistence/src/main/java/com/netflix/conductor/aurora/AuroraMetricsDAO.java
@@ -7,6 +7,7 @@ import com.netflix.conductor.common.metadata.events.EventHandler;
 import com.netflix.conductor.common.metadata.tasks.Task;
 import com.netflix.conductor.common.run.Workflow;
 import com.netflix.conductor.core.config.Configuration;
+import com.netflix.conductor.core.execution.ParametersUtils;
 import com.netflix.conductor.dao.MetadataDAO;
 import com.netflix.conductor.dao.MetricsDAO;
 
@@ -69,6 +70,7 @@ public class AuroraMetricsDAO extends AuroraBaseDAO implements MetricsDAO {
 
 	private static final String VERSION = "\\.\\d+\\.\\d+"; // covers '.X.Y' where X and Y any number/digit
 	private static final String PREFIX = "deluxe.conductor";
+	private static ParametersUtils pu = new ParametersUtils();
 	private MetadataDAO metadataDAO;
 
 	@Inject
@@ -375,6 +377,7 @@ public class AuroraMetricsDAO extends AuroraBaseDAO implements MetricsDAO {
 	private Set<String> getSubjects() {
 		return getHandlers().stream()
 			.map(eh -> eh.getEvent().split(":")[1]) // 0 - event bus, 1 - subject, 2 - queue
+			.map(event -> pu.replace(event).toString())
 			.collect(Collectors.toSet());
 	}
 
