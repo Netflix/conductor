3128a74f1227b14c4cdf248c4e6d9ccc9da0ddcb
Oleksiy Lysak <olysak@luxoft.com>
2019-10-29 10:11:50 +0200

    ONECOND-845 Login Functionality in ConductorUI

--- ui/src/api/v1.js
+++ ui/src/api/v1.js
@@ -6,7 +6,6 @@ const TLD = process.env.TLD;
 const WF_SERVER = process.env.WF_SERVER;
 const WF_SERVICE = process.env.WF_SERVICE;
 const APP_VERSION = process.env.APP_VERSION || "1.7.6-deluxe-ui";
-const AUTH = process.env.conductor_auth_url;
 
 router.get('/status', async (req, res, next) => {
   try {
@@ -19,19 +18,17 @@ router.get('/status', async (req, res, next) => {
 router.get('/dependencies', async (req, res, next) => {
   try {
     let conductor = WF_SERVICE || WF_SERVER;
+    let keycloak = "keycloak.service." + TLD;
     let vault = "vault.service." + TLD;
+    let auth = "auth.service." + TLD;
 
-    let endpoints = [conductor, vault];
-    if (AUTH) {
-      endpoints.push(AUTH);
-    }
+    let endpoints = [conductor, keycloak, vault, auth];
     let dependencies = [
       {name: "conductor-server", version: "v1", scheme: "http", external: false},
+      {name: "keycloak", version: "v1", scheme: "http", external: false},
       {name: "vault", version: "v1", scheme: "http", external: false},
+      {name: "auth", version: "v1", scheme: "http", external: false}
     ];
-    if (AUTH) {
-      dependencies.push({name: "auth", version: "v1", scheme: "https", external: false});
-    }
 
     res.status(200).send({version: APP_VERSION, endpoints, dependencies});
   } catch(err) {
