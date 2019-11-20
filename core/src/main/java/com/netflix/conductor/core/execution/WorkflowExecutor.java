810e2189a39fd8a424285fd83d5cb99213d46588
Manan Bhatt <manan16489@gmail.com>
2019-11-20 14:03:57 +0530

    Revert "Force workflow deletion from redis based on environment config."

--- core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutor.java
+++ core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutor.java
@@ -662,9 +662,7 @@ public class WorkflowExecutor {
         queueDAO.remove(DECIDER_QUEUE, workflow.getWorkflowId());    //remove from the sweep queue
 
         //Remove workflow from redis irrespective of status listener.
-        if (config.getForceWorkflowDeletionFromRedis() || workflow.getWorkflowDefinition().isWorkflowStatusListenerEnabled()) {
-            workflowStatusListener.onWorkflowCompleted(workflow);
-        }
+        workflowStatusListener.onWorkflowCompleted(workflow);
     }
 
     public void terminateWorkflow(String workflowId, String reason) {
@@ -760,9 +758,7 @@ public class WorkflowExecutor {
         Monitors.recordWorkflowTermination(workflow.getWorkflowName(), workflow.getStatus(), workflow.getOwnerApp());
 
         //Remove workflow from redis irrespective of status listener.
-        if (config.getForceWorkflowDeletionFromRedis()|| workflow.getWorkflowDefinition().isWorkflowStatusListenerEnabled()) {
-            workflowStatusListener.onWorkflowCompleted(workflow);
-        }
+        workflowStatusListener.onWorkflowTerminated(workflow);
     }
 
     /**
