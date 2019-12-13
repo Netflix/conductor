6e426be0f6ee4914a9faf84cb62009ac8d1234c9
James DeMichele <james.demichele@redfin.com>
2019-12-13 16:56:44 -0600

    Reverting change around LeftMenu clicks

--- ui/src/api/wfegraph.js
+++ ui/src/api/wfegraph.js
@@ -29,34 +29,22 @@ class Workflow2Graph {
         tt.reasonForIncompletion,
         task: tt
       };
-<<<<<<< HEAD
-
-      if(tt.type === 'FORK'){
-=======
       if(tt.taskType === 'FORK'){
->>>>>>> 6b7f28a283639587bbfcb87eb39301d173dbbf22
         let wfts = [];
         let forkedTasks = tt.inputData && tt.inputData.forkedTasks || [];
         forkedTasks.forEach(ft =>{
-          wfts.push({name: ft, taskReferenceName: ft, type: 'SIMPLE'});
+          wfts.push({name: ft, referenceTaskName: ft, type: 'SIMPLE'});
         });
-        forks[tt.taskReferenceName] = wfts;
+        forks[tt.referenceTaskName] = wfts;
       }
-<<<<<<< HEAD
-=======
 
->>>>>>> 6b7f28a283639587bbfcb87eb39301d173dbbf22
     }, this);
 
     let nodes = [];
     let vertices = {};
-<<<<<<< HEAD
-
-=======
     wfe.tasks.forEach(t => {
       this.executedTasks[t.referenceTaskName] = {status: t.status, input: t.inputData, output: t.outputData, taskType: t.taskType, reasonForIncompletion: t.reasonForIncompletion, task: t};
     })
->>>>>>> 6b7f28a283639587bbfcb87eb39301d173dbbf22
     // Go through each JOIN in the workflow and build up a mapping to their joinOn data
     this.joinOnTaskMapping = {};
     metaTasks.forEach(t => {
