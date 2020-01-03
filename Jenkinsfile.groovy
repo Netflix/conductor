0411043dfb8d4d31389e178a587a34eac9f27142
hoa.phan <hoa.phan@foxsports.com.au>
2020-01-03 15:56:31 +1100

    gimme back my es

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,7 +21,7 @@ pipeline {
                 HELM_RELEASE = "$PREVIEW_NAMESPACE".toLowerCase()
                 CONDUCTOR_API = "http://conductor-server.${PREVIEW_NAMESPACE}.dev.cluster.foxsports-gitops-prod.com.au/api"
                 EXPECT_WORKFLOW_CREATION_TIME_SECS = "10"
-                EXPECT_WORKFLOW_COMPLETION_TIME_SECS = "240"
+                EXPECT_WORKFLOW_COMPLETION_TIME_SECS = "300"
             }
             steps {
                 container('maven') {
