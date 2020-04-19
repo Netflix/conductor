040563ca5dd4f3ddfac954255923cd9f0cdcab64
Oleksiy Lysak <olysak@luxoft.com>
2020-04-19 15:15:33 +0300

    ONECOND-1521 TEST - Charter Load Test - Conductor made 2 POST packaging calls for the same WF

--- postgresql-persistence/src/main/java/com/netflix/conductor/aurora/sql/Query.java
+++ postgresql-persistence/src/main/java/com/netflix/conductor/aurora/sql/Query.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
