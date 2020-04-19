fc68857eed238ca312a5bff314ae8813d9723908
Oleksiy Lysak <olysak@luxoft.com>
2020-04-19 18:39:39 +0300

    ONECOND-1521 TEST - Charter Load Test - Conductor made 2 POST packaging calls for the same WF

--- postgresql-persistence/src/main/resources/initial_schema.sql
+++ postgresql-persistence/src/main/resources/initial_schema.sql
@@ -284,4 +284,4 @@ create table queue_message
 create unique index queue_name_msg on queue_message (queue_name, message_id);
 alter table queue_message
     add constraint queue_name_msg unique using index queue_name_msg;
-create index queue_name_deliver_on on queue_message (deliver_on);
+create index queue_name_deliver_on on queue_message (queue_name, deliver_on);
