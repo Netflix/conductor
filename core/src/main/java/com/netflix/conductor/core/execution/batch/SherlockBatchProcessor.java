c3be076f9682702aa139b03513512d75fefc2fb9
sangeetanadgir <Sangeeta.Nadgir@bydeluxe.com>
2020-04-24 14:06:54 +0530

    ONECOND-1544

--- core/src/main/java/com/netflix/conductor/core/execution/batch/SherlockBatchProcessor.java
+++ core/src/main/java/com/netflix/conductor/core/execution/batch/SherlockBatchProcessor.java
@@ -165,13 +165,13 @@ public class SherlockBatchProcessor extends AbstractBatchProcessor {
 				try {
 					if (task.getTaskId().equalsIgnoreCase(carried.getTaskId())) {
 						logger.info("batch task execution completed.workflowId=" + task.getWorkflowInstanceId() +
-							",correlationId=" + task.getCorrelationId() +
+							",correlationId=" + task.getCorrelationId() +",uniquenessGroup=" + group.getKey() +
 							",taskId=" + task.getTaskId() +
 							",taskReferenceName=" + task.getReferenceTaskName() +
 							",url=" + effectiveUrl + ",response code=" + response.statusCode + ",response=" + response.body);
 					} else {
 						logger.info("batch task execution completed.carriedTaskId=" + carried.getTaskId() +
-							",carriedCorrelationId=" + carried.getCorrelationId() +
+							",carriedCorrelationId=" + carried.getCorrelationId() +",uniquenessGroup=" + group.getKey() +
 							",workflowId=" + task.getWorkflowInstanceId() +
 							",correlationId=" + task.getCorrelationId() +
 							",taskId=" + task.getTaskId() +
