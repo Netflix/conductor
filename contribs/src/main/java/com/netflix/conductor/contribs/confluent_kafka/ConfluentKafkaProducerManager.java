38ffafc075f4cddd5bf2c8f669c36e5d9a3d7fc6
Manan B Bhatt <manan16489@gmail.com>
2020-04-28 18:45:32 +0530

    log line

--- contribs/src/main/java/com/netflix/conductor/contribs/confluent_kafka/ConfluentKafkaProducerManager.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/confluent_kafka/ConfluentKafkaProducerManager.java
@@ -137,10 +137,12 @@ public class ConfluentKafkaProducerManager {
 	private String getTaskKey(String taskDefName, String clusterType, String workflowName) {
 		String userName = configuration.getProperty( KAFKA_PREFIX +  "__" + taskDefName.toUpperCase() +  "__" + ClusterType.valueOf(clusterType).key , "");
 		if (userName != "") {
+			logger.info("Secret found for task" + taskDefName + " for cluster " + clusterType);
 			return userName;
 		}
 		userName = configuration.getProperty( KAFKA_PREFIX +  "__" + workflowName.toUpperCase() + "__" + taskDefName.toUpperCase() +  "__" + ClusterType.valueOf(clusterType).key , "");
 		 if (userName != "") {
+			 logger.info("Secret found for task" + taskDefName + " in workflow " + workflowName + " for cluster " + clusterType);
 			return userName;
 		}
 		return "";
@@ -149,10 +151,12 @@ public class ConfluentKafkaProducerManager {
 	private String getTaskSecret(String taskDefName, String clusterType, String workflowName) {
 		String password = configuration.getProperty( KAFKA_PREFIX +  "__" + taskDefName.toUpperCase() +  "__" + ClusterType.valueOf(clusterType).secret , "");
 		if (password != "") {
+			logger.info("Secret found for task" + taskDefName + " for cluster " + clusterType);
 			return password;
 		}
 		password = configuration.getProperty( KAFKA_PREFIX +  "__" + workflowName.toUpperCase() + "__" + taskDefName.toUpperCase() +  "__" + ClusterType.valueOf(clusterType).secret , "");
 		if (password != "") {
+			logger.info("Secret found for task" + taskDefName + " in workflow " + workflowName + " for cluster " + clusterType);
 			return password;
 		}
 		return "";
