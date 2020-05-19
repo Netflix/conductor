7c01ea9dc0b813005e0c00031591f79b8dae01e3
Rahul Gupta <ragupta4@cisco.com>
2020-05-19 21:57:02 +0530

    add prefer header for async request and add timeout
    Signed-off-by: Rahul Gupta <ragupta4@cisco.com>

--- contribs/src/main/java/com/netflix/conductor/contribs/publisher/RestClient.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/publisher/RestClient.java
@@ -3,6 +3,7 @@ package com.netflix.conductor.contribs.publisher;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,9 +12,12 @@ public class RestClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(RestClient.class);
     //private static final String URL = "http://bullwinkle.default:7979/v1/workflow";
     private static final String URL = "http://bullwinkle.default.svc.cluster.local:7979/v1";
-    private static final String DOMAIN_GROUP = "X-Starship-DomainGroup";
-    private static final String ACCOUNT_COOKIE = "x-barracuda-account";
+    private static final String HEADER_DOMAIN_GROUP = "X-Starship-DomainGroup";
+    private static final String HEADER_ACCOUNT_COOKIE = "x-barracuda-account";
+    private static final String HEADER_PREFER = "Prefer";
+    private static final String HEADER_PREFER_VALUE = "respond-async";
     private Client client = Client.create();
+    // default timeout value for all requests
 
     public Client getClient(String notificationType) {
         return client;
@@ -27,13 +31,18 @@ public class RestClient {
         LOGGER.info("URL: " + uri);
         LOGGER.info("Input: " + input);
         LOGGER.info("domainGroupId: " + domainGroupMoId);
+        int timeoutInSeconds = 10;
+        client.setConnectTimeout(1000 * timeoutInSeconds);
+        client.setReadTimeout(1000 * timeoutInSeconds );
+
         try {
             WebResource webResource = client.resource(uri);
             ClientResponse response = webResource.type("application/json")
-                    .header(DOMAIN_GROUP, domainGroupMoId)
-                    .header(ACCOUNT_COOKIE, accountMoId)
+                    .header(HEADER_DOMAIN_GROUP, domainGroupMoId)
+                    .header(HEADER_ACCOUNT_COOKIE, accountMoId)
+                    .header(HEADER_PREFER, HEADER_PREFER_VALUE)
                     .post(ClientResponse.class, input);
-            if (response.getStatus() != 200) {
+            if (response.getStatus() != HttpStatus.SC_ACCEPTED) {
                 throw new RuntimeException("Failed : HTTP error code : "
                         + response.getStatus());
             }
