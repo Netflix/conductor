15dd72a0970d0fdfdfd60babd3203d362a1f4e03
Manan <manan.bhatt@swiggy.in>
2020-06-04 16:22:38 +0530

    prop fix.

--- core/src/main/java/com/netflix/conductor/core/config/Configuration.java
+++ core/src/main/java/com/netflix/conductor/core/config/Configuration.java
@@ -227,7 +227,7 @@ public interface Configuration {
     }
 
     default boolean getKafkaIndexEnable() {
-        return getBooleanProperty(KAFKA_INDEX_ENABLE, true);
+        return getBooleanProperty(KAFKA_INDEX_ENABLE, false);
     }
 
     default boolean isForceWorkflowDeletionEnabled() {
