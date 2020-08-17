be3f2ff43da059e1474baad63d80628cac2dac37
Eli Sherer <elish@payoneer.com>
2020-08-17 16:20:09 +0300

    JSON_JQ_TRANSFORM - complete system task development

---
+++ core/src/test/java/com/netflix/conductor/core/execution/mapper/JsonJQTransformTaskMapperTest.java
@@ -0,0 +1,107 @@
+package com.netflix.conductor.core.execution.mapper;
+
+import com.netflix.conductor.common.metadata.tasks.Task;
+import com.netflix.conductor.common.metadata.tasks.TaskDef;
+import com.netflix.conductor.common.metadata.workflow.TaskType;
+import com.netflix.conductor.common.metadata.workflow.WorkflowDef;
+import com.netflix.conductor.common.metadata.workflow.WorkflowTask;
+import com.netflix.conductor.common.run.Workflow;
+import com.netflix.conductor.core.execution.ParametersUtils;
+import com.netflix.conductor.core.utils.IDGenerator;
+import com.netflix.conductor.dao.MetadataDAO;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+public class JsonJQTransformTaskMapperTest {
+
+    private ParametersUtils parametersUtils;
+    private MetadataDAO metadataDAO;
+
+    @Before
+    public void setUp() {
+        parametersUtils = mock(ParametersUtils.class);
+        metadataDAO = mock(MetadataDAO.class);
+    }
+
+    @Test
+    public void getMappedTasks() throws Exception {
+
+        WorkflowTask taskToSchedule = new WorkflowTask();
+        taskToSchedule.setName("json_jq_transform_task");
+        taskToSchedule.setType(TaskType.JSON_JQ_TRANSFORM.name());
+        taskToSchedule.setTaskDefinition(new TaskDef("json_jq_transform_task"));
+
+        Map<String, Object> taskInput = new HashMap<>();
+        taskInput.put("in1", new String[] {"a", "b"});
+        taskInput.put("in2", new String[] {"c", "d"});
+        taskInput.put("queryExpression", "{ out: (.in1 + .in2) }");
+        taskToSchedule.setInputParameters(taskInput);
+
+        String taskId = IDGenerator.generate();
+
+        WorkflowDef  wd = new WorkflowDef();
+        Workflow w = new Workflow();
+        w.setWorkflowDefinition(wd);
+
+        TaskMapperContext taskMapperContext = TaskMapperContext.newBuilder()
+                .withWorkflowDefinition(wd)
+                .withWorkflowInstance(w)
+                .withTaskDefinition(new TaskDef())
+                .withTaskToSchedule(taskToSchedule)
+                .withTaskInput(taskInput)
+                .withRetryCount(0)
+                .withTaskId(taskId)
+                .build();
+
+        List<Task> mappedTasks = new JsonJQTransformTaskMapper(parametersUtils, metadataDAO).getMappedTasks(taskMapperContext);
+
+        assertEquals(1, mappedTasks.size());
+        assertNotNull(mappedTasks);
+        assertEquals(TaskType.JSON_JQ_TRANSFORM.name(), mappedTasks.get(0).getTaskType());
+    }
+
+    @Test
+    public void getMappedTasks_WithoutTaskDef() throws Exception {
+        WorkflowTask taskToSchedule = new WorkflowTask();
+        taskToSchedule.setName("json_jq_transform_task");
+        taskToSchedule.setType(TaskType.JSON_JQ_TRANSFORM.name());
+
+        Map<String, Object> taskInput = new HashMap<>();
+        taskInput.put("in1", new String[] {"a", "b"});
+        taskInput.put("in2", new String[] {"c", "d"});
+        taskInput.put("queryExpression", "{ out: (.in1 + .in2) }");
+        taskToSchedule.setInputParameters(taskInput);
+
+        String taskId = IDGenerator.generate();
+
+        WorkflowDef  wd = new WorkflowDef();
+        Workflow w = new Workflow();
+        w.setWorkflowDefinition(wd);
+
+        TaskMapperContext taskMapperContext = TaskMapperContext.newBuilder()
+                .withWorkflowDefinition(wd)
+                .withWorkflowInstance(w)
+                .withTaskDefinition(null)
+                .withTaskToSchedule(taskToSchedule)
+                .withTaskInput(taskInput)
+                .withRetryCount(0)
+                .withTaskId(taskId)
+                .build();
+
+        List<Task> mappedTasks = new JsonJQTransformTaskMapper(parametersUtils, metadataDAO).getMappedTasks(taskMapperContext);
+
+        assertEquals(1, mappedTasks.size());
+        assertNotNull(mappedTasks);
+        assertEquals(TaskType.JSON_JQ_TRANSFORM.name(), mappedTasks.get(0).getTaskType());
+    }
+
+}
