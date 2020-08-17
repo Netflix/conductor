16c0f049661c6ac2228d551917b0ce7ba13883b1
Eli Sherer <elish@payoneer.com>
2020-08-17 17:03:19 +0300

    JSON_JQ_TRANSFORM - remove unused dep from test

--- core/src/test/java/com/netflix/conductor/core/execution/mapper/JsonJQTransformTaskMapperTest.java
+++ core/src/test/java/com/netflix/conductor/core/execution/mapper/JsonJQTransformTaskMapperTest.java
@@ -12,7 +12,6 @@ import com.netflix.conductor.dao.MetadataDAO;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
