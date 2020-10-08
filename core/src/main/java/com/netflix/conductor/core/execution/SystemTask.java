30acfdb59d5c6430e751ea5814cb2b1a1999b78e
Pradeep <pradeep.palat01@gmail.com>
2020-10-07 17:53:19 -0700

    Added comment to fix bad checkin

--- core/src/main/java/com/netflix/conductor/core/execution/SystemTask.java
+++ core/src/main/java/com/netflix/conductor/core/execution/SystemTask.java
@@ -152,10 +152,12 @@ public class SystemTask extends Task {
 		st.getInputData().put("subWorkflowName", subWorkflowName);
 		st.getInputData().put("subWorkflowVersion", subWorkflowVersion);
 		st.getInputData().put("workflowInput", input);
+		//Add Job Priority here?
 		st.setTaskId(taskId);
 		st.setStatus(Status.SCHEDULED);
 		st.setWorkflowTask(taskToSchedule);
 		return st;
+
 	}
 	
 	public static Task userDefined(Workflow workflow, String taskId, WorkflowTask taskToSchedule, Map<String, Object> input, TaskDef taskDef, int retryCount) {
