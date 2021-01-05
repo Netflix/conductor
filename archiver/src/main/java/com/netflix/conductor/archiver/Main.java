62044e57a4394e235378656798f106198d0dfd5c
sangeetanadgir <Sangeeta.Nadgir@bydeluxe.com>
2021-01-05 14:44:26 +0530

    ONECOND-1749-Remove code which cleanups decider queue (QueueMessageJob)

--- archiver/src/main/java/com/netflix/conductor/archiver/Main.java
+++ archiver/src/main/java/com/netflix/conductor/archiver/Main.java
@@ -60,7 +60,6 @@ public class Main {
 		jobs.add(new EventPubsJob(dataSource));
 		jobs.add(new WorkflowJob(dataSource));
 		jobs.add(new DbLogJob(dataSource));
-		jobs.add(new QueueMessageJob(dataSource));
 
 		// Run jobs in threads
 		CountDownLatch countDown = new CountDownLatch(jobs.size());
