a8a5f53787f9ad5b82bc31fe66b88feb4543f462
Mahesh Yaddanapudi <zzzmahesh@gmail.com>
2021-05-18 01:29:41 +0530

    Added Docker Compose YAML for Security with Keycloak as security provider

--- server/src/main/java/com/netflix/conductor/Conductor.java
+++ server/src/main/java/com/netflix/conductor/Conductor.java
@@ -12,29 +12,29 @@
  */
 package com.netflix.conductor;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.util.StringUtils;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import java.io.IOException;
-import java.util.Properties;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.util.StringUtils;
 
 // Prevents from the datasource beans to be loaded, AS they are needed only for specific databases.
 // In case that SQL database is selected this class will be imported back in the appropriate database persistence module.
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class , SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class Conductor {
 
     private static final Logger log = LoggerFactory.getLogger(Conductor.class);
 
     public static void main(String[] args) throws IOException {
         loadExternalConfig();
-        loadExternalConfigFromArgs(args);
-
+       
         SpringApplication.run(Conductor.class, args);
     }
 
@@ -61,24 +61,5 @@ public class Conductor {
             }
         }
     }
-    
-    private static void loadExternalConfigFromArgs(String[] args) throws IOException {
-    	if(null!=args && args.length>0) {
-    		for(String configFile: args)
-    		{
-    			if (!StringUtils.isEmpty(configFile)) {
-    	            FileSystemResource resource = new FileSystemResource(configFile);
-    	            if (resource.exists()) {
-    	                Properties properties = new Properties();
-    	                properties.load(resource.getInputStream());
-    	                properties.forEach((key, value) -> System.setProperty((String) key, (String) value));
-    	                log.info("Loaded {} properties from {}", properties.size(), configFile);
-    	            }else {
-    	                log.warn("Ignoring {} since it does not exist", configFile);
-    	            }
-    	        }
-    		}
-    	}
-    }
-
+   
 }
