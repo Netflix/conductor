33870d3952753c79b91c344290e8f451b3178a1a
u447 <rick.fishman@bcbsfl.com>
2021-07-08 08:13:25 -0400

    change imports for new file locations

--- postgres-persistence/src/main/java/com/netflix/conductor/postgres/dao/Query.java
+++ postgres-persistence/src/main/java/com/netflix/conductor/postgres/dao/Query.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package com.netflix.conductor.dao.postgres;
+package com.netflix.conductor.postgres.dao;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
