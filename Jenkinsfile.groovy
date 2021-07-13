95ae078acfd52fddacbe3b58e4152afe4229e3b1
Javier Azabache <1798489+javierazabache@users.noreply.github.com>
2021-07-13 14:35:21 +1000

    No need for component tests here... Conductor already has a full battery of tests (that are disabled for some reason... need to re-enable them later)

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -29,7 +29,6 @@ pipeline {
                 container('maven') {
                     sh "echo **************** PREVIEW_VERSION: $PREVIEW_VERSION , PREVIEW_NAMESPACE: $PREVIEW_NAMESPACE, HELM_RELEASE: $HELM_RELEASE"
                     sh "echo $PREVIEW_VERSION > PREVIEW_VERSION"
-                    sh "cp -v server/src/main/resources/kitchensinkpreview.json server/src/main/resources/kitchensink.json"
                     sh "skaffold version && ./gradlew build -w -x test -x :conductor-client:findbugsMain "
                     sh "export VERSION=$PREVIEW_VERSION && skaffold build -f skaffold-server.yaml && skaffold build -f skaffold-ui.yaml"
                     // Comment out skaffold build all
@@ -52,31 +51,6 @@ pipeline {
                       sh "kubectl describe pods -n $PREVIEW_NAMESPACE"
                       sh "echo '************************************************\n' && cat values.yaml"
                       sh "printenv | sort && kubectl get pods -n $PREVIEW_NAMESPACE"
-                    }
-                }
-            }
-        }
-
-        stage('Component Test') {
-            when {
-                branch 'PR-*'
-            }
-            environment {
-                PREVIEW_NAMESPACE = "$APP_NAME-$BRANCH_NAME".toLowerCase()
-                CONDUCTOR_API = "http://conductor-server.${PREVIEW_NAMESPACE}.jx.gitops-prod.streamotion.gitops.com.au/api"
-            }
-            steps {
-                container('maven') {
-                    dir('client/python') {
-                        // ///DO some loadtest:
-                        // 1. make sure conductor preview up & running
-                        // 2. upload some workflow & tasks in conductor server
-                        // 3. simulate some producers to generate X amount of workflow
-                        // 4. simulate some workers to consume all the tasks (do concurrency)
-                        // 5. assert we dont have any hanging tasks and all workflows completed
-                        // sh "python kitchensink_workers.py > worker.log &"
-                        // sh "python load_test_kitchen_sink.py"
-                        // todo: fix these
                     }
                 }
             }
@@ -144,8 +118,8 @@ pipeline {
                         // promote through all 'Auto' promotion Environments
                         //sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=staging --version \$(cat ../../VERSION)"
                         //sh "jx promote -b --no-poll=true --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=production --version \$(cat ../../VERSION)"
-                        sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=conductor-staging --version \$(cat ../../VERSION)"
-                        sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=conductor-production --version \$(cat ../../VERSION)"
+                        //sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=conductor-staging --version \$(cat ../../VERSION)"
+                        //sh "jx promote -b --no-poll=true  --helm-repo-url=$CHART_REPOSITORY --no-poll=true --no-merge=true --no-wait=true --env=conductor-production --version \$(cat ../../VERSION)"
                     }
                 }
             }
