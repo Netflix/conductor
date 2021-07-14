77035c45cc4c5f5f99d1f1d6f5239220bee30516
Javier Azabache <1798489+javierazabache@users.noreply.github.com>
2021-07-14 16:28:59 +1000

    Let's see if we can run docker in docker

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,27 @@ pipeline {
 
     environment {
         GRADLE_OPTS = '-Xmx3g -XX:MaxMetaspaceSize=512m -Dorg.gradle.daemon=false'
-        TESTCONTAINERS_HOST_OVERRIDE = 'docker.svc.local'
+        container = 'docker'
+    }
+
+    stages {
+        stage('Setup Docker in Docker') {
+            container('maven') {
+                sh '''yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \\
+                      yum update -y && \\
+                      yum install -y docker-ce docker-ce-cli containerd.io'''
+
+                sh '''update-alternatives --set iptables  /usr/sbin/iptables-legacy || true && \\
+                      update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy || true && \\
+                      update-alternatives --set arptables /usr/sbin/arptables-legacy || true'''
+
+                sh '''set -x && \\
+                      groupadd --system dockremap && \\
+                      adduser --system -g dockremap dockremap && \\
+                      echo 'dockremap:165536:65536' >> /etc/subuid && \\
+                      echo 'dockremap:165536:65536' >> /etc/subgid'''
+            }
+        }
     }
 
     stages {
