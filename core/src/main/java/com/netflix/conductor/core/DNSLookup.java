de3a6163f7f1f54d49e4d78638a3886392962123
Oleksiy Lysak <oleksiy.lysak@dxc.com>
2021-07-21 15:16:37 +0300

    ONECOND-1916 Conductor makes redundant SD lookups (#813)
    * ONECOND-1916 Conductor makes redundant SD lookups
    
    * ONECOND-1916 Conductor makes redundant SD lookups

--- core/src/main/java/com/netflix/conductor/core/DNSLookup.java
+++ core/src/main/java/com/netflix/conductor/core/DNSLookup.java
@@ -1,120 +1,83 @@
 package com.netflix.conductor.core;
+
 import com.netflix.conductor.service.MetricService;
-import org.apache.commons.lang3.ArrayUtils;
 import org.xbill.DNS.*;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 
 /**
  * Created by hhwang on 6/15/2017.
  */
 public class DNSLookup {
-    public DNSLookup(){}
+	public DNSLookup() {
+	}
 
-    public static String lookup(String service) {
-        long sd_start_time = System.currentTimeMillis();
-        long sd_lookup_time = -1;
-        try {
-            DNSLookup lookup = new DNSLookup();
-            DNSLookup.DNSResponses responses = lookup.lookupService(service);
-            if (responses != null && ArrayUtils.isNotEmpty(responses.getResponses())) {
-                String address = responses.getResponses()[0].getAddress();
-                int port = responses.getResponses()[0].getPort();
-                sd_lookup_time = System.currentTimeMillis() - sd_start_time;
-                return "http://" + address + ":" + port;
-            }
-            return null;
-        } finally {
-            // Service Discovery Metric
-            MetricService
-                    .getInstance()
-                    .serviceDiscovery(service, sd_lookup_time);
-        }
-    }
+	public static String lookup(String service) {
+		long sd_start_time = System.currentTimeMillis();
+		long sd_lookup_time = -1;
+		try {
+			DNSLookup lookup = new DNSLookup();
+			DNSResponse dnsResponse = lookup.lookupService(service);
+			if (dnsResponse != null) {
+				String address = dnsResponse.getAddress();
+				int port = dnsResponse.getPort();
+				sd_lookup_time = System.currentTimeMillis() - sd_start_time;
+				return "http://" + address + ":" + port;
+			}
+			return null;
+		} finally {
+			// Service Discovery Metric
+			MetricService
+					.getInstance()
+					.serviceDiscovery(service, sd_lookup_time);
+		}
+	}
 
-    public DNSResponses lookupService(String query) {
-        DNSResponses responses = new DNSResponses();
-        try {
-            Lookup lookup = new Lookup(query, Type.SRV);
-            Cache cache = Lookup.getDefaultCache(DClass.IN);
-            cache.clearCache();
-            lookup.setCache(null);
-            Record[] records = lookup.run();
-            if (records != null) {
-                for (Record record : records) {
-                    SRVRecord srv = (SRVRecord) record;
+	public DNSResponse lookupService(String query) {
+		try {
+			Lookup lookup = new Lookup(query, Type.SRV);
+			Cache cache = Lookup.getDefaultCache(DClass.IN);
+			cache.clearCache();
+			lookup.setCache(null);
+			Record[] records = lookup.run();
+			if (records != null) {
+				for (Record record : records) {
+					SRVRecord srv = (SRVRecord) record;
 
-                    String hostname = srv.getTarget().toString().replaceFirst("\\.$", "");
-                    InetAddress address = Address.getByName(hostname);
-                    int port = srv.getPort();
-                    DNSResponse r = new DNSResponse(hostname, port);
-                    r.setAddress(address.getHostAddress());
-                    responses.addResponse(r);
-                }
-            }
-        } catch (TextParseException | UnknownHostException e) {
-            e.printStackTrace();
-        }
-        return responses;
-    }
+					String hostname = srv.getTarget().toString().replaceFirst("\\.$", "");
+					InetAddress address = Address.getByName(hostname);
+					int port = srv.getPort();
+					return new DNSResponse(hostname, port, address.getHostAddress());
+				}
+			}
+		} catch (TextParseException | UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-    public class DNSResponse {
+	private static class DNSResponse {
+		private final String hostname;
+		private final String address;
+		private final int port;
 
-        String hostname;
-        String address;
-        int port;
+		public DNSResponse(String hostname, int port, String address) {
+			this.hostname = hostname;
+			this.address = address;
+			this.port = port;
+		}
 
-        public DNSResponse(String hostname, int port) {
-            this.hostname = hostname;
-            this.port = port;
-        }
+		public String getHostName() {
+			return this.hostname;
+		}
 
-        public void setHostName(String hostname) {
-            this.hostname = hostname;
-        }
-        public String getHostName() {
-            return this.hostname;
-        }
+		public String getAddress() {
+			return this.address;
+		}
 
-        public void setAddress(String address) {
-            this.address = address;
-        }
-        public String getAddress() {
-            return this.address;
-        }
-
-
-        public void setPort(int port) {
-            this.port = port;
-        }
-        public int getPort() {
-            return this.port;
-        }
-    }
-
-    public class DNSResponses {
-
-        DNSResponse[] responses;
-
-        public DNSResponses() {};
-
-        public void setResponses(DNSResponse[] r) {
-            this.responses = r;
-        }
-        public DNSResponse[] getResponses() {
-            return this.responses;
-        }
-
-        public void addResponse(DNSResponse response) {
-            if (responses == null) {
-                responses = new DNSResponse[1];
-            }
-            else {
-                responses = Arrays.copyOf(responses, responses.length+1);
-            }
-            responses[responses.length-1] = response;
-        }
-    }
+		public int getPort() {
+			return this.port;
+		}
+	}
 }
