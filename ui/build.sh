63fb4f1d1fd92dc7ba71a3771c23415ded720e86
Peter Lau <peterl@netflix.com>
2021-09-21 12:45:13 -0700

    Merge conductor-ui-oss

--- ui/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-newt exec yarn install
-newt exec yarn build
--- ui/dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-#Disable browser auto opening in CRA since it will be the wrong port
-BROWSER=none yarn start
