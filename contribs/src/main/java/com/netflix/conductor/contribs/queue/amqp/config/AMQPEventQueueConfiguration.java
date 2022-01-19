5f60f3bacb8da1bd441b93f26172138bac4c0150
Thangella, Venkat <Venkat.Thangella@netapp.com>
2022-01-19 04:51:15 -0500

    Resolved conflicts

--- contribs/src/main/java/com/netflix/conductor/contribs/queue/amqp/config/AMQPEventQueueConfiguration.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/queue/amqp/config/AMQPEventQueueConfiguration.java
@@ -31,53 +31,53 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(name = "conductor.event-queues.amqp.enabled", havingValue = "true")
 public class AMQPEventQueueConfiguration {
 
-    private enum QUEUE_TYPE {
-        AMQP_QUEUE("amqp_queue"), AMQP_EXCHANGE("amqp_exchange");
+	private enum QUEUE_TYPE {
+		AMQP_QUEUE("amqp_queue"), AMQP_EXCHANGE("amqp_exchange");
 
-        private final String type;
+		private final String type;
 
-        QUEUE_TYPE(String type) {
-            this.type = type;
-        }
+		QUEUE_TYPE(String type) {
+			this.type = type;
+		}
 
-        public String getType() {
-            return type;
-        }
-    }
+		public String getType() {
+			return type;
+		}
+	}
 
-    @Bean
-    public EventQueueProvider amqpEventQueueProvider(AMQPEventQueueProperties properties) {
-        return new AMQPEventQueueProvider(properties, QUEUE_TYPE.AMQP_QUEUE.getType(), false);
-    }
+	@Bean
+	public EventQueueProvider amqpEventQueueProvider(AMQPEventQueueProperties properties) {
+		return new AMQPEventQueueProvider(properties, QUEUE_TYPE.AMQP_QUEUE.getType(), false);
+	}
 
-    @Bean
-    public EventQueueProvider amqpExchangeEventQueueProvider(AMQPEventQueueProperties properties) {
-        return new AMQPEventQueueProvider(properties, QUEUE_TYPE.AMQP_EXCHANGE.getType(), true);
-    }
+	@Bean
+	public EventQueueProvider amqpExchangeEventQueueProvider(AMQPEventQueueProperties properties) {
+		return new AMQPEventQueueProvider(properties, QUEUE_TYPE.AMQP_EXCHANGE.getType(), true);
+	}
 
-    @ConditionalOnProperty(name = "conductor.default-event-queue.type", havingValue = "amqp")
-    @Bean
-    public Map<Status, ObservableQueue> getQueues(ConductorProperties conductorProperties,
-        AMQPEventQueueProperties properties) {
-        String stack = "";
-        if (conductorProperties.getStack() != null && conductorProperties.getStack().length() > 0) {
-            stack = conductorProperties.getStack() + "_";
-        }
-        final boolean useExchange = properties.isUseExchange();
+	@ConditionalOnProperty(name = "conductor.default-event-queue.type", havingValue = "amqp")
+	@Bean
+	public Map<Status, ObservableQueue> getQueues(ConductorProperties conductorProperties,
+			AMQPEventQueueProperties properties) {
+		String stack = "";
+		if (conductorProperties.getStack() != null && conductorProperties.getStack().length() > 0) {
+			stack = conductorProperties.getStack() + "_";
+		}
+		final boolean useExchange = properties.isUseExchange();
 
-        Status[] statuses = new Task.Status[]{Status.COMPLETED, Status.FAILED};
-        Map<Status, ObservableQueue> queues = new HashMap<>();
-        for (Status status : statuses) {
-            String queuePrefix = StringUtils.isBlank(properties.getListenerQueuePrefix())
-                ? conductorProperties.getAppId() + "_amqp_notify_" + stack
-                : properties.getListenerQueuePrefix();
+		Status[] statuses = new Task.Status[] { Status.COMPLETED, Status.FAILED };
+		Map<Status, ObservableQueue> queues = new HashMap<>();
+		for (Status status : statuses) {
+			String queuePrefix = StringUtils.isBlank(properties.getListenerQueuePrefix())
+					? conductorProperties.getAppId() + "_amqp_notify_" + stack
+					: properties.getListenerQueuePrefix();
 
-            String queueName = queuePrefix + status.name();
+			String queueName = queuePrefix + status.name();
 
-            final ObservableQueue queue = new Builder(properties).build(useExchange, queueName);
-            queues.put(status, queue);
-        }
+			final ObservableQueue queue = new Builder(properties).build(useExchange, queueName);
+			queues.put(status, queue);
+		}
 
-        return queues;
-    }
+		return queues;
+	}
 }
