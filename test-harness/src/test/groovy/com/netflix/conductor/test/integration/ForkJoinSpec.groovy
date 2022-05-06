bc222070fc223a52e2dc0ac07f0294eed50835df
Jiaofen Xu <jxu@netflix.com>
2022-05-06 14:49:09 -0700

    Fix test

--- test-harness/src/test/groovy/com/netflix/conductor/test/integration/ForkJoinSpec.groovy
+++ test-harness/src/test/groovy/com/netflix/conductor/test/integration/ForkJoinSpec.groovy
@@ -12,11 +12,11 @@
  */
 package com.netflix.conductor.test.integration
 
-import com.netflix.conductor.common.metadata.workflow.RerunWorkflowRequest
 import org.springframework.beans.factory.annotation.Autowired
 
 import com.netflix.conductor.common.metadata.tasks.Task
 import com.netflix.conductor.common.metadata.tasks.TaskDef
+import com.netflix.conductor.common.metadata.workflow.RerunWorkflowRequest
 import com.netflix.conductor.common.run.Workflow
 import com.netflix.conductor.core.execution.tasks.SubWorkflow
 import com.netflix.conductor.test.base.AbstractSpecification
