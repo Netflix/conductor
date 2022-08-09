e41241590aa25bedf60827dd9f7628800d961174
yaroslav.halchenko <YH-Oksi@@22>
2022-08-09 18:57:43 +0300

    ONECOND-2031 Flyway migration support in conductor (create index if not exist)

--- postgresql-persistence/src/main/resources/db/migrations/V1_1__create_index_for_json_data_workflow_ids.sql
+++ postgresql-persistence/src/main/resources/db/migrations/V1_1__create_index_for_json_data_workflow_ids.sql
@@ -1,1 +1,2 @@
-CREATE INDEX workflow_json_data_workflow_ids_gin_idx ON workflow USING gin (json_data_workflow_ids jsonb_path_ops);
+CREATE INDEX IF NOT EXISTS workflow_json_data_workflow_ids_gin_idx
+    ON workflow USING gin (json_data_workflow_ids jsonb_path_ops);
