7db4e8569424d9a2fbc258f9b7d726debb410ae8
astelmashenko <andrey.stelmashenko@gmail.com>
2022-09-22 10:56:54 +0300

    removing event handlers once it is removed from storage.

--- core/src/main/java/com/netflix/conductor/core/events/DefaultEventQueueManager.java
+++ core/src/main/java/com/netflix/conductor/core/events/DefaultEventQueueManager.java
@@ -151,6 +151,22 @@ public class DefaultEventQueueManager extends LifecycleAwareComponent implements
                     .peek(Lifecycle::start)
                     .forEach(this::listen);
 
+            Set<String> removed = new HashSet<>(eventToQueueMap.keySet());
+            removed.removeAll(events);
+            removed.forEach(key -> {
+                ObservableQueue queue = eventToQueueMap.remove(key);
+                try {
+                    queue.stop();
+                } catch (Exception e) {
+                    LOGGER.error("Failed to stop queue: " + queue, e);
+                }
+            });
+
+            LOGGER.debug("Event queues: {}", eventToQueueMap.keySet());
+            LOGGER.debug("Stored queue: {}", events);
+            LOGGER.debug("Removed queue: {}", removed);
+
+
         } catch (Exception e) {
             Monitors.error(getClass().getSimpleName(), "refresh");
             LOGGER.error("refresh event queues failed", e);
