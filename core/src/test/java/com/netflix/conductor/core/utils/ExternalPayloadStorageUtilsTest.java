b63736ac82542d7adddd53e04dd777f875bc9f33
Manan Bhatt <manan16489@gmail.com>
2022-11-02 15:06:57 +0530

    revert

--- core/src/test/java/com/netflix/conductor/core/utils/ExternalPayloadStorageUtilsTest.java
+++ core/src/test/java/com/netflix/conductor/core/utils/ExternalPayloadStorageUtilsTest.java
@@ -201,8 +201,8 @@ public class ExternalPayloadStorageUtilsTest {
         TaskModel task = new TaskModel();
         task.setInputData(new HashMap<>());
 
-        assertThrows(TerminateWorkflowException.class, ()-> externalPayloadStorageUtils.failTask(
-                task, ExternalPayloadStorage.PayloadType.TASK_INPUT, "error"));
+        externalPayloadStorageUtils.failTask(
+                task, ExternalPayloadStorage.PayloadType.TASK_INPUT, "error");
         assertNotNull(task);
         assertTrue(task.getInputData().isEmpty());
         assertEquals(FAILED_WITH_TERMINAL_ERROR, task.getStatus());
@@ -213,8 +213,8 @@ public class ExternalPayloadStorageUtilsTest {
         TaskModel task = new TaskModel();
         task.setOutputData(new HashMap<>());
 
-        assertThrows(TerminateWorkflowException.class, ()-> externalPayloadStorageUtils.failTask(
-                task, ExternalPayloadStorage.PayloadType.TASK_OUTPUT, "error"));
+        externalPayloadStorageUtils.failTask(
+                task, ExternalPayloadStorage.PayloadType.TASK_OUTPUT, "error");
         assertNotNull(task);
         assertTrue(task.getOutputData().isEmpty());
         assertEquals(FAILED_WITH_TERMINAL_ERROR, task.getStatus());
