c0559ac67cea803758e11dbd8d529a7d66001f2a
manan164 <manan16489@gmail.com>
2022-12-13 00:51:54 +0530

    Event task retry fix

--- core/src/main/java/com/netflix/conductor/core/events/SimpleActionProcessor.java
+++ core/src/main/java/com/netflix/conductor/core/events/SimpleActionProcessor.java
@@ -127,7 +127,7 @@ public class SimpleActionProcessor implements ActionProcessor {
             List<TaskModel> loopOverTaskList =
                     workflow.getTasks().stream()
                             .filter(
-                                    t ->
+                                    t -> t.isLoopOverTask() &&
                                             TaskUtils.removeIterationFromTaskRefName(
                                                             t.getReferenceTaskName())
                                                     .equals(taskRefName))
